55c747dab2a91b809490058df1bb670428303ef8
Cosmin Oancea <coancea76@yahoo.com>
2015-02-24 15:17:23 +0100

    made contract3 def short

--- GenericPricing/Orig_COpenMp/Contracts.h
+++ GenericPricing/Orig_COpenMp/Contracts.h
@@ -92,9 +92,47 @@ L21:
 
 
 // LENGTHY CONTRACT
-
 inline 
 void trajectory_contract3( // BARRIER
+        const UINT&     model_num,  // the index of the current model
+        const UINT&     num_under,  // the number of underlyings
+        const UINT&     num_cash_flows,
+        const UINT&     num_pricers,// the number of deterministic procers 
+        const REAL*     md_discts,  // [num_models][num_scash_flow] discounts
+        const REAL*     md_detvals, // [num_models, num_det_pricers]  pricers 
+        const REAL*     inst_traj,  // [num_dates, num_under] current trajectory
+              double*   vhat        // [model_num] Accumulated per-model price
+) {
+    bool goto_L40, x3309 = false; 
+    REAL mult  = 0.0;
+    for(int i=0; (i<367 && (!x3309)); i++) {
+        x3309 = x3309 || 
+                    (underlyings(i,0) <= 2630.6349999999998) ||
+                    (underlyings(i,1) <= 8288.) ||
+                    (underlyings(i,2) <= 840. ) ;
+    }
+
+    trajectory_inner( num_cash_flows, model_num, 0, 100., md_discts, vhat );
+
+    goto_L40 = x3309 && 
+                ( (underlyings(366,0) < 3758.05) ||
+                  (underlyings(366,1) < 11840. ) ||
+                  (underlyings(366,2) < 1200.  ) );
+
+    mult = (goto_L40) ? 1.0 : 0.0;
+
+    const REAL min_amount = fmin( (underlyings(366,1) / 11840.) - 1., 
+                                  fmin( (underlyings(366,2) / 1200.  ) - 1., 
+                                        (underlyings(366,0) / 3758.05) - 1. ) );
+
+    const REAL amount = 1000. * ( 1. + mult*min_amount ) ;
+
+    trajectory_inner( num_cash_flows, model_num, 1, amount, md_discts, vhat );
+}
+
+#if 0
+inline 
+void trajectory_contract3_OLD( // BARRIER
         const UINT&     model_num,  // the index of the current model
         const UINT&     num_under,  // the number of underlyings
         const UINT&     num_cash_flows,
@@ -1231,6 +1269,8 @@ L40:
 
 	return;
 }
+
+#endif
 
 #define CONTRACT_NUM 2
 
