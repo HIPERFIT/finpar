94bcf7965f666a17d850b68be538a0b87600acc2
Troels Henriksen <athas@sigkill.dk>
2015-06-19 13:33:44 +0200

    First draft of fixing CalibGA.

--- benchmarks/CalibGA/implementations/cpp_openmp_outer/SwapCalib.cpp
+++ benchmarks/CalibGA/implementations/cpp_openmp_outer/SwapCalib.cpp
@@ -5,8 +5,8 @@
 /***    (sequential) by LexiFi        ***/
 /***   and a sequential Python implem ***/
 /***    using a different genetic alg.***/
-/***    by Christian Andreetta        ***/  
-/****************************************/ 
+/***    by Christian Andreetta        ***/
+/****************************************/
 
 #include "Util.h"
 #include "Constants.h"
@@ -15,19 +15,19 @@
 
 int main()
 {
-    REAL wg_a  = 0.0, wg_b   = 0.0, wg_sigma  = 0.0, 
+    REAL wg_a  = 0.0, wg_b   = 0.0, wg_sigma  = 0.0,
          wg_nu = 0.0, wg_rho = 0.0, wg_logLik = 0.0;
     REAL* calib_arr = NULL;
 
     printf("\n// Running Original (CPU Parallel) Swaption-Calibration Benchmark\n");
 
     readDataSet(    POP_SIZE, MCMC_LOOPS,
-                    NUM_SWAP_QUOTES, SwaptionQuotes, 
+                    NUM_SWAP_QUOTES, SwaptionQuotes,
                     NUM_HERMITE, HermiteCoeffs, HermiteWeights,
                     NUM_SOBOL_BITS, SobolDirVct
-               ); 
+               );
 
-    unsigned long int elapsed = 0;
+    unsigned long int elapsed_usec = 0;
     {   // Main Computational Kernel
         struct timeval t_start, t_end, t_diff;
         gettimeofday(&t_start, NULL);
@@ -36,18 +36,28 @@ int main()
 
         gettimeofday(&t_end, NULL);
         timeval_subtract(&t_diff, &t_end, &t_start);
-        elapsed = t_diff.tv_sec*1e6+t_diff.tv_usec;
+        elapsed_usec = t_diff.tv_sec*1e6+t_diff.tv_usec;
     }
 
     {   // validation and writeback of the result
+        FILE* runtime = fopen("runtime.txt", "w");
+        FILE* result = fopen("result.json", "w");
         const int Ps = get_CPU_num_threads();
-        bool is_valid = validate( wg_logLik, calib_arr, NUM_SWAP_QUOTES );
-        writeStatsAndResult(    is_valid, wg_a, wg_b, wg_sigma, wg_nu, wg_rho, 
-                                wg_logLik, calib_arr, NUM_SWAP_QUOTES, 
-                                false, Ps, elapsed );        
+        fprintf(runtime, "%d\n", elapsed_usec / 1000);
+        fclose(runtime);
+
+        write_scal(result, &wg_a);
+        write_scal(result, &wg_b);
+        write_scal(result, &wg_sigma);
+        write_scal(result, &wg_nu);
+        write_scal(result, &wg_rho);
+        write_scal(result, &wg_logLik);
+        write_2Darr(result, calib_arr, static_cast<int>(NUM_SWAP_QUOTES), 3);
+
+        fclose(result);
     }
 
-	return 1;
+    return 0;
 }
 
 
@@ -59,4 +69,3 @@ void test_all() {
     test_pricer_of_swaption();
 }
 #endif
-
---
+++ benchmarks/CalibGA/lib/include/ParseInput.h
@@ -0,0 +1,102 @@
+#ifndef PARSE_INPUT
+#define PARSE_INPUT
+
+#include "ParserC.h"
+#include <assert.h>
+#include <math.h>
+#include <algorithm>
+#include "GenAlgUtil.h"
+
+//using namespace std;
+
+#if WITH_FLOAT
+    #define read_real read_float
+#else
+    #define read_real read_double
+#endif
+
+/***********************************/
+/********** READ DATA SET **********/
+/***********************************/
+
+void readDataSet(   UINT&  pop_size,
+                    UINT&  num_conv_iters,   
+                    UINT&  num_swap_quotes, 
+                    REAL*& swaption_quotes,  // [num_swap_quotes,4]
+                    
+                    UINT&  num_hermitians,
+                    REAL*& hermite_coefs,   // [num_hermite]
+                    REAL*& hermite_weights,  // [num_hermite]
+                
+                    UINT&  num_sobol_bits,
+                    int*&  sobol_dirs_vct   // [num_sobol_bits]
+) {
+    int64_t shape[3];
+    bool atr_ok = true;
+
+    if ( read_int( static_cast<UINT*>( &pop_size       ) ) ||
+         read_int( static_cast<UINT*>( &num_conv_iters ) )  ) {
+            fprintf(stderr, "Syntax error when reading population size or convergence-loop count!\n");
+            exit(1);
+    }
+    atr_ok  = (pop_size >= 64) && (num_conv_iters >= 100);
+    assert(atr_ok && "Population size < 64 OR convergence-loop count < 100!\n");
+
+    { // swaption quotes
+        if ( read_int( static_cast<UINT*>( &num_swap_quotes ) ) ) {
+            fprintf(stderr, "Syntax error when reading NUM_SWAP_QUOTES.\n");
+            exit(1);
+        }
+        atr_ok  = num_swap_quotes > 0;
+        assert(atr_ok && "Number of swaptions LESS or equal to ZERO!\n");
+
+        if ( read_array(sizeof(REAL), read_real, (void**)&swaption_quotes, shape, 2) ) {
+            fprintf(stderr, "Syntax error when reading the swaption quotes.\n");
+            exit(1);
+        }
+        atr_ok = ( shape[1] == 4 && shape[0] == num_swap_quotes );
+        assert(atr_ok && "Incorrect shape of the swaption quotes!");
+    }
+
+    { // hermitean coefficients
+        if ( read_int( static_cast<UINT*>( &num_hermitians ) ) ) {
+            fprintf(stderr, "Syntax error when reading number of hermitians.\n");
+            exit(1);
+        }
+        atr_ok  = num_hermitians > 0;
+        assert(atr_ok && "Number of hermitians LESS or equal to ZERO!\n");
+
+        if ( read_array(sizeof(REAL), read_real, (void**)&hermite_coefs, shape, 1) ) {
+            fprintf(stderr, "Syntax error when reading the hermitian coeficients.\n");
+            exit(1);
+        }
+        atr_ok = ( shape[0] == num_hermitians );
+        assert(atr_ok && "Incorrect shape of the hermitian coefficients!");
+
+        if ( read_array(sizeof(REAL), read_real, (void**)&hermite_weights, shape, 1) ) {
+            fprintf(stderr, "Syntax error when reading the hermitian weights.\n");
+            exit(1);
+        }
+        atr_ok = ( shape[0] == num_hermitians );
+        assert(atr_ok && "Incorrect shape of the hermitian weights!");
+    }
+
+    { // sobol direction vectors!
+        if ( read_int( static_cast<UINT*>( &num_sobol_bits ) ) ) {
+            fprintf(stderr, "Syntax error when reading NUM_SOBOL_BITS.\n");
+            exit(1);
+        }
+        atr_ok  = num_sobol_bits > 0;
+        assert(atr_ok && "Number of sobol bits LESS or equal to ZERO!\n");
+
+        if ( read_array(sizeof(int), read_int, (void**)&sobol_dirs_vct, shape, 1) ) {
+            fprintf(stderr, "Syntax error when reading sobol direction vectors.\n");
+            exit(1);
+        }
+        atr_ok = ( shape[0] == num_sobol_bits );
+        assert( atr_ok && "Incorrect shape of sobol direction vectors!" );
+    }
+}
+
+#endif // PARSE_INPUT
+
