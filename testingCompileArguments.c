4b3785627bc78ea49202cf26ec629d22fe1bbe2e
Thor <thorkilk@gmail.com>
2015-07-16 17:35:04 +0200

    removed thitesting file

--- testingCompileArguments.c
+++
@@ -1,9 +0,0 @@
-// THIS FILE CAN BE COMPILED WITH: 
-// gcc -o testingCompileArguments testingCompileArguments.c -D STRING='"Hello World"'
-// This tells the preprocessor to replace STRING by the value on the RHS of the equality sign.
-#include <stdio.h>
-
-int main (int argc, char *argv[]) {
-  printf("String is: %s\n", STRING);
-  return 0;
-}
