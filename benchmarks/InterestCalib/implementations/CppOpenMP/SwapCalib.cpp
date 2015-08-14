115a8ee6fb0e92baf900ec4fa0488b7396d9113c
Troels Henriksen <athas@sigkill.dk>
2015-08-14 11:17:32 +0200

    Port of InterestCalib CppOpenMP.

--- benchmarks/InterestCalib/implementations/CppOpenMP/SwapCalib.cpp
+++ benchmarks/InterestCalib/implementations/CppOpenMP/SwapCalib.cpp
@@ -5,29 +5,30 @@
 /***    (sequential) by LexiFi        ***/
 /***   and a sequential Python implem ***/
 /***    using a different genetic alg.***/
-/***    by Christian Andreetta        ***/  
-/****************************************/ 
+/***    by Christian Andreetta        ***/
+/****************************************/
 
 #include "Util.h"
 #include "Constants.h"
 #include "ParseInput.h"
 #include "GenAlgFlat.h"
+#include "WriteResult.h"
 
 int main()
 {
-    REAL wg_a  = 0.0, wg_b   = 0.0, wg_sigma  = 0.0, 
+    REAL wg_a  = 0.0, wg_b   = 0.0, wg_sigma  = 0.0,
          wg_nu = 0.0, wg_rho = 0.0, wg_logLik = 0.0;
     REAL* calib_arr = NULL;
 
     printf("\n// Running Original (CPU Parallel) Swaption-Calibration Benchmark\n");
 
     readDataSet(    POP_SIZE, MCMC_LOOPS,
-                    NUM_SWAP_QUOTES, SwaptionQuotes, 
+                    NUM_SWAP_QUOTES, SwaptionQuotes,
                     NUM_HERMITE, HermiteCoeffs, HermiteWeights,
                     NUM_SOBOL_BITS, SobolDirVct
-               ); 
+               );
 
-    unsigned long int elapsed = 0;
+    unsigned long int elapsed_usec = 0;
     {   // Main Computational Kernel
         struct timeval t_start, t_end, t_diff;
         gettimeofday(&t_start, NULL);
@@ -36,18 +37,29 @@ int main()
 
         gettimeofday(&t_end, NULL);
         timeval_subtract(&t_diff, &t_end, &t_start);
-        elapsed = t_diff.tv_sec*1e6+t_diff.tv_usec;
+        elapsed_usec = t_diff.tv_sec*1e6+t_diff.tv_usec;
     }
 
     {   // validation and writeback of the result
+        FILE* runtime = fopen(getenv("HIPERMARK_RUNTIME"), "w");
+        FILE* result = fopen(getenv("HIPERMARK_RESULT"), "w");
         const int Ps = get_CPU_num_threads();
-        bool is_valid = validate( wg_logLik, calib_arr, NUM_SWAP_QUOTES );
-        writeStatsAndResult(    is_valid, wg_a, wg_b, wg_sigma, wg_nu, wg_rho, 
-                                wg_logLik, calib_arr, NUM_SWAP_QUOTES, 
-                                false, Ps, elapsed );        
+        fprintf(runtime, "%d\n", elapsed_usec / 1000);
+        fclose(runtime);
+
+        writeResult(result,
+                    wg_a,
+                    wg_b,
+                    wg_sigma,
+                    wg_nu,
+                    wg_rho,
+                    wg_logLik,
+                    calib_arr);
+
+        fclose(result);
     }
 
-	return 1;
+    return 0;
 }
 
 
@@ -59,4 +71,3 @@ void test_all() {
     test_pricer_of_swaption();
 }
 #endif
-
