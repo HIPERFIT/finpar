ee6656dfc176934f4701d924406677c8a86fbc80
Troels Henriksen <athas@sigkill.dk>
2015-08-21 10:01:09 +0200

    Move real_t stuff into its own header.

--- benchmarks/LocVolCalib/lib/include/Constants.h
+++ benchmarks/LocVolCalib/lib/include/Constants.h
@@ -1,42 +1,10 @@
 #ifndef   CONSTANTS_H
 #define   CONSTANTS_H
 
+#include "real.h"
+
 #define REAL3_CT 4
 #define TRANSPOSE_UV 1
-
-#ifdef REAL_IS_DOUBLE
-
-/* Some OpenCL implementations require us to define a pragma if we
-   want to use double-precision numbers. */
-#ifdef __OPENCL_VERSION__
-#pragma OPENCL EXTENSION cl_khr_fp64 : enable
-#endif
-
-typedef double real_t;
-#define REAL_FLAG "REAL_IS_DOUBLE"
-
-#ifdef __OPENCL_VERSION__
-#define real4_t double4
-#define real3_t double3
-#define real2_t double2
-#endif
-
-#elif REAL_IS_FLOAT
-
-typedef float real_t;
-#define REAL_FLAG "REAL_IS_FLOAT"
-
-#ifdef __OPENCL_VERSION__
-#define real4_t float4
-#define real3_t float3
-#define real2_t float2
-#endif
-
-#else
-
-#error "Must set REAL_IS_DOUBLE or REAL_IS_FLOAT."
-
-#endif
 
 #define WARP            (1<<lgWARP)
 
